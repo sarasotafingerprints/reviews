6a938fd8b18f95a583bbe715522759a9c9dca378
sarasotafingerprints <121450556+sarasotafingerprints@users.noreply.github.com>
2023-02-01 09:12:23 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import { Widget } from '@typeform/embed-react'
+
+export default function Home() {
+  return (
+    <Widget id="StIuBpvq" style={{ width: '100%', height: '100vh' }} className="my-form" inlineOnMobile={true} hidden={{ kiosk: '1'}}/>
+  )
+}
