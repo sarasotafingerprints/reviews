636246a215cc41b5d3f6bb409185abe9618cb5f8
sarasotafingerprints <mike@sarasotafingerprints.com>
2023-02-01 09:16:58 -0500

    Update index.js
    Changed form id

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import { Widget } from '@typeform/embed-react'
 
 export default function Home() {
   return (
-    <Widget id="StIuBpvq" style={{ width: '100%', height: '100vh' }} className="my-form" inlineOnMobile={true} hidden={{ kiosk: '1'}}/>
+    <Widget id="fRAagsGY" style={{ width: '100%', height: '100vh' }} className="my-form" inlineOnMobile={true}/>
   )
 }
