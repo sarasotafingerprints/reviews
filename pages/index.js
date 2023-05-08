0ba95488b8cd26b194c22b66923aad8c10e2a60a
sarasotafingerprints <mike@sarasotafingerprints.com>
2023-05-08 11:32:06 -0400

    80vh
    80vh

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import { Widget } from '@typeform/embed-react'
 
 export default function Home() {
   return (
-    <Widget id="fRAagsGY" style={{ width: '100%', height: '100vh' }} className="my-form" transitiveSearchParams={["email", "first_name"]}/>
+    <Widget id="fRAagsGY" style={{ width: '100%', height: '80vh' }} className="my-form" transitiveSearchParams={["email", "first_name"]}/>
   )
 }
